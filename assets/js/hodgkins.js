b7326f09f32785559cb30fa22817779f8f5887e0
aa0 <dev@aao.fyi>
2022-07-15 03:21:34 -0400

    Add theme and dyslexic toggles

--- assets/js/hodgkins.js
+++ assets/js/hodgkins.js
@@ -1,3 +1,60 @@
+// Config Functions
+var config = {
+	// Element prefix
+	prefix: "conf-",
+
+	// buttonCycle
+	buttonCycle: function(cfTarget) {
+		var cfStateMax = document.getElementById(this.prefix + cfTarget).children.length - 1;
+		var lsvState = localStorage.getItem(cfTarget);
+		// Increment to next state.
+		if (lsvState >= cfStateMax) {
+			lsvState = 0;
+		} else if (lsvState === null) {
+			lsvState = 1;
+		} else {
+			lsvState++;
+		}
+		localStorage.setItem(cfTarget, lsvState);
+		this.setRoot(cfTarget, lsvState);
+		this.setIcon(cfTarget, lsvState);
+	},
+
+	// check
+	check: function(cfTarget) {
+		let lsvState = localStorage.getItem(cfTarget);
+		if (lsvState !== null) {
+			this.setRoot(cfTarget, lsvState);
+			this.setIcon(cfTarget, lsvState);
+		}
+	},
+
+	// setIcon
+	setIcon: function(cfTarget, cfState) {
+		var cfFeat = document.getElementById(this.prefix + cfTarget);
+		for (let cf = 0; cf < cfFeat.children.length; cf++) {
+			if (cf === parseInt(cfState)) {
+				cfFeat.children[cf].classList.remove("d-none");
+			} else {
+				cfFeat.children[cf].classList.add("d-none");
+			}
+		}
+	},
+
+	// setRoot
+	setRoot: function(cfTarget, cfState) {
+		// Set attribute state.
+		document.documentElement.setAttribute(cfTarget, cfState);
+	},
+}
+
+// Page Load
+document.addEventListener("DOMContentLoaded", function() {
+	// Config checks.
+	config.check("theme");
+	config.check("dyslexic");
+});
+
 // Bootstrap Collapse
 document.querySelectorAll(".portfolio-item").forEach(item => {
 	// On Expand
